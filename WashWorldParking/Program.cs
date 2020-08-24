9367bcbb19dc26606715bab205128583917d7d9d
Jan Andreasen <51907684+jaa2019@users.noreply.github.com>
2020-08-24 13:52:50 +0200

    Nu med file logger

--- WashWorldParking/Program.cs
+++ WashWorldParking/Program.cs
@@ -6,7 +6,29 @@ namespace WashWorldParking
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu();
+            UTIL.FileLogger.WriteToLog("Tissemand");
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("╔══════════════════════════╗");
+            Console.WriteLine("║  Waterworld | Parkworld  ║");
+            Console.WriteLine("╠══════════════════════════╣");
+            Console.WriteLine("║      Please select:      ║");
+            Console.WriteLine("║      [W]ash car          ║");
+            Console.WriteLine("║      [O]pen account      ║");
+            Console.WriteLine("║      [S]ee account       ║");
+            Console.WriteLine("║      [H]eureka!          ║");
+            Console.WriteLine("║                          ║");
+            Console.WriteLine("║      [P]ark              ║");
+            Console.WriteLine("║      [A]add time         ║");
+            Console.WriteLine("║      [R]evoke ticket     ║");
+            Console.WriteLine("║      [C]ancel parking    ║");
+            Console.WriteLine("║                          ║");
+            Console.WriteLine("║      [X] Exit            ║");
+            Console.WriteLine("╚══════════════════════════╝");
         }
     }
 }
---
+++ WashWorldParking/UTIL/FileLogger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace WashWorldParking.UTIL
+{
+    static class FileLogger
+    {
+        static string fileName = "logs.bnk";
+
+        public static void WriteToLog(string logmessage)
+        {
+            string writeMsg = DateTime.Now.ToString() + " " + logmessage + "\n";
+            File.AppendAllText(fileName, writeMsg);
+        }
+
+        public static string ReadFromLog()
+        {
+            DirectoryInfo findLog = new DirectoryInfo(Environment.CurrentDirectory);
+            foreach (FileInfo filer in findLog.GetFiles())
+            {
+                if (filer.Extension == ".bnk")
+                {
+                    return File.ReadAllText(filer.FullName);
+                }
+            }
+            return "Der er ikke fundet nogen log.";
+        }
+    }
+}
