01b2ede5084133ae1cdfecd9e22455f945e1a297
Jan Andreasen <51907684+jaa2019@users.noreply.github.com>
2020-09-02 08:15:48 +0200

    Finishing touches

---
+++ WashWorldParking/MDL/WashEnum.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WashWorldParking.MDL
+{
+    public enum WashEnum
+    {
+        [Display(Name ="Bronze Wash")] Bronze_Wash,
+        [Display(Name = "Silver wash")] Silver_Wash,
+        [Display(Name = "Golden Shower")] Golden_Shower
+    }
+}
---
+++ WashWorldParking/MDL/WashMembers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace WashWorldParking.MDL
+{
+    public class WashMembers
+    {
+        public string LPlate { get; set; }
+        public string CCard { get; set; }
+        public string EMail { get; set; }
+        public int WashType { get; set; }
+        public string WashName { get; set; }
+
+        public WashMembers(string lPlate, string cCard, string eMail, int wType)
+        {
+            LPlate = lPlate;
+            CCard = cCard;
+            EMail = eMail;
+            WashType = wType;
+            if (wType == 1) WashName = "Bronze wash";
+            if (wType == 2) WashName = "Silver wash";
+            if (wType == 3) WashName = "Golden shower";
+        }
+    }
+}
